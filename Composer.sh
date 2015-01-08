50185f66f6c41b393488d9ff13e607ecbd0b6410
Gary B. Genett <me@garybgenett.net>
2015-01-08 09:47:21 -0800

    [devel :: 2015-01-08T09:47:21-0800]

--- Composer.sh
+++ Composer.sh
@@ -2,5 +2,5 @@
 _CMS="${PWD}"
 _SYS="Linux"; [ -n "${MSYSTEM}" ] && _SYS="Msys"
 PATH="${_CMS}/bin/${_SYS}/usr/bin:${_CMS}/.home/.coreutils:${PATH}"
-exec make --makefile Makefile --debug="a" BUILD_PLAT="Linux" BUILD_ARCH="x86_64" COMPOSER_PROGS_USE="1" shell
+exec make --makefile Makefile --debug="a" COMPOSER_PROGS_USE="1" shell
 # end of file
---
+++ testing/.composer.mk
@@ -0,0 +1,1 @@
+override COMPOSER_OTHER ?= /.g/_data/_builds/composer.64bit/Native
