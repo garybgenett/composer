4fa79d1d49306ab773c234117a74329276173c89
Gary B. Genett <me@garybgenett.net>
2015-01-08 09:47:21 -0800

    [devel :: 2015-01-08T09:47:21-0800]

--- Composer.bat
+++ Composer.bat
@@ -2,5 +2,5 @@
 set _CMS=%~dp0
 set _SYS=Msys
 set PATH=%_CMS%/bin/%_SYS%/usr/bin;%_CMS%/.home/.coreutils;%PATH%
-start /b make --makefile Makefile --debug="a" BUILD_PLAT="Linux" BUILD_ARCH="x86_64" COMPOSER_PROGS_USE="1" shell-msys
+start /b make --makefile Makefile --debug="a" COMPOSER_PROGS_USE="1" shell-msys
 :: end of file
