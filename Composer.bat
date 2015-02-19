93c3eb1a50d3743e3a51b2dcb6f6b0750ff183e1
Gary B. Genett <me@garybgenett.net>
2015-02-19 03:12:56 -0800

    [Composer CMS v1.4 :: Thu, 19 Feb 2015 03:12:56 -0800]

--- Composer.bat
+++ Composer.bat
@@ -5,9 +5,12 @@ set _DIR=%_CMS%/.home
 set _BIN=
 set _BIN=%_BIN%%_DIR%/bin/;
 set _BIN=%_BIN%%_DIR%/msys64/usr/bin;
+set _BIN=%_BIN%%_DIR%/msys32/usr/bin;
 set _BIN=%_BIN%%_CMS%/bin/%_SYS%/usr/bin;
 set PATH=%_BIN%%PATH%
-set _OPT=
-if not exist %_DIR%/msys64/usr/bin set _OPT=%_OPT% COMPOSER_PROGS_USE="1"
-start /b make --makefile Makefile --debug="a" BUILD_PLAT="%_SYS%" %_OPT% shell-msys
+set _OPT=1
+if exist %_DIR%/bin set _OPT=
+if exist %_DIR%/msys64/usr/bin set _OPT=
+if exist %_DIR%/msys32/usr/bin set _OPT=
+start /b make --makefile Makefile --debug="a" COMPOSER_PROGS_USE="%_OPT%" BUILD_PLAT="%_SYS%" shell-msys
 :: end of file
--- Composer.sh
+++ Composer.sh
@@ -5,9 +5,12 @@ _DIR=${_CMS}/.home
 _BIN=
 _BIN=${_BIN}${_DIR}/bin:
 _BIN=${_BIN}${_DIR}/msys64/usr/bin:
+_BIN=${_BIN}${_DIR}/msys32/usr/bin:
 _BIN=${_BIN}${_CMS}/bin/${_SYS}/usr/bin:
 PATH=${_BIN}${PATH}
-_OPT=
-[ ! -d ${_DIR}/msys64/usr/bin ] && _OPT="${_OPT} COMPOSER_PROGS_USE=\"1\""
-exec make --makefile Makefile --debug="a" BUILD_PLAT="${_SYS}" ${_OPT} shell
+_OPT=1
+[ -d ${_DIR}/bin ] && _OPT=
+[ -d ${_DIR}/msys64/usr/bin ] && _OPT=
+[ -d ${_DIR}/msys32/usr/bin ] && _OPT=
+exec make --makefile Makefile --debug="a" COMPOSER_PROGS_USE="${_OPT}" BUILD_PLAT="${_SYS}" shell
 # end of file
