2feb1b61166206455e61d56b610b8bbf2ae960bb
Gary B. Genett <me@garybgenett.net>
2015-03-27 17:30:13 -0700

    [Composer CMS v2.0 :: Fri, 27 Mar 2015 17:30:13 -0700]

--- bin/Msys/msys2_shell.bat
+++ bin/Msys/msys2_shell.bat
@@ -3,7 +3,7 @@ if not defined MSYSTEM set MSYSTEM=MSYS32
 set _CMS=%~dp0
 set _BIN=/usr/bin
 set _OPT=
-set _OPT=%_OPT% --title ">> Composer CMS v1.4 :: MSYS2 Shell"
+set _OPT=%_OPT% --title ">> Composer CMS v2.0 :: MSYS2 Shell"
 set _OPT=%_OPT% --icon %_CMS%/../../icon.ico
 set _OPT=%_OPT% --exec %_BIN%/bash
 start /b %_CMS%%_BIN%/mintty %_OPT%
