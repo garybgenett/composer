67d844d4bfbe5873c3c210f467657f1ea5cdc687
Gary B. Genett <me@garybgenett.net>
2015-04-01 12:36:19 -0700

    [Composer CMS v2.0 :: Wed, 01 Apr 2015 12:36:19 -0700]

--- Composer.sh
+++ Composer.sh
@@ -1,15 +1,19 @@
 # sh
 _CMS="${PWD}"
 _SYS="Linux"; [ -n "${MSYSTEM}" ] && _SYS="Msys"
-_BIN="/usr/bin"
-_DIR="${_CMS}/.home"
-_PTH=
-_PTH="${_PTH}${_DIR}${_BIN}:"
-_PTH="${_PTH}${_DIR}/msys32${_BIN}:"
-_PTH="${_PTH}${_CMS}/bin/${_SYS}${_BIN}:"
-PATH="${_PTH}${PATH}"
+_BIN="usr/bin"
+_ABD="${_CMS}/.home"
+_PRG="${_CMS}/bin/Linux"
 _OPT="1"
-[ -d "${_DIR}${_BIN}" ] && _OPT=
-[ -d "${_DIR}/msys32${_BIN}" ] && _OPT=
-exec make --makefile Makefile --debug="a" COMPOSER_PROGS_USE="${_OPT}" BUILD_PLAT="${_SYS}" shell
+if [ -e "${_ABD}/${_BIN}" ]; then
+PATH="${_ABD}/${_BIN}:${PATH}"
+_OPT=
+elif [ -e "${_ABD}/msys32/${_BIN}" ]; then
+PATH="${_ABD}/msys32/${_BIN}:${PATH}"
+_OPT=
+elif [ -e "${_CMS}/bin/${_SYS}/${_BIN}" ]; then
+PATH="${_CMS}/bin/${_SYS}/${_BIN}:${PATH}"
+_OPT=1
+fi
+exec make --makefile Makefile --debug="a" COMPOSER_ESCAPES="0" COMPOSER_PROGS_USE="${_OPT}" BUILD_PLAT="${_SYS}" BUILD_ARCH= shell
 # end of file
